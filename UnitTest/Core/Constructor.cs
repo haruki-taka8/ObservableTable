4a1e721972e879f870deb976462a908b6e8e61a2
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-04-27 15:20:18 +0800

    Fix history not being recorded if a empty constructor is used to initialize the table Unit tests are modified to reflect this change.

---
+++ UnitTest/Core/Constructor.cs
@@ -0,0 +1,39 @@
+﻿using ObservableTable.Core;
+
+namespace UnitTest.Core;
+
+[TestClass]
+public class Constructor
+{
+    [TestMethod]
+    public void Ctor_Empty_Nothing()
+    {
+        ObservableTable<string> actual = new();
+
+        // Check if table is empty
+        Assert.IsTrue(actual.Headers.Count == 0);
+        Assert.IsTrue(actual.Records.Count == 0);
+
+        // Check if recordTransaction is true
+        Assert.IsTrue(actual.UndoCount == 0);
+        actual.InsertRow(0, Array.Empty<string>());
+        Assert.IsTrue(actual.UndoCount == 1);
+    }
+
+    [TestMethod]
+    public void Ctor_HeadersAndRecords_Inserted()
+    {
+        var actual = Helper.GetSampleTable();
+
+        // Check if headers & records exists
+        Assert.IsTrue(Enumerable.SequenceEqual(actual.Headers, new string[] { "A0", "B0", "C0" }));
+        Assert.IsTrue(actual.Records.Count == 2);
+        Assert.IsTrue(Enumerable.SequenceEqual(actual.Records[0], new string[] { "A1", "B1", "C1" }));
+        Assert.IsTrue(Enumerable.SequenceEqual(actual.Records[1], new string[] { "A2", "B2", "C2" }));
+
+        // Check if recordTransaction is true
+        Assert.IsTrue(actual.UndoCount == 0);
+        actual.InsertRow(0, Array.Empty<string>());
+        Assert.IsTrue(actual.UndoCount == 1);
+    }
+}
