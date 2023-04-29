55e0b9ca5be7629a3f263efdb6b509693fb52184
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-04-29 09:13:45 +0800

    Simplify ColumnEdit<T> constructor call

--- ObservableTable/Core/History.cs
+++ ObservableTable/Core/History.cs
@@ -20,7 +20,7 @@ internal interface IEdit
         }
         if (this is ColumnEdit<T> column)
         {
-            return new ColumnEdit<T>(column.Parity, column.IsInsert, column.Index, column.Header, column.Values);
+            return new ColumnEdit<T>(column.Parity, column.IsInsert, column.Index, column);
         }
         var cell = (CellEdit<T>)this;
         return new CellEdit<T>(cell.Parity, cell.Row, cell.Column, cell.Value);
@@ -46,12 +46,6 @@ internal class ColumnEdit<T> : Column<T>, IEdit
     public int Parity { get; init; }
     public int Index { get; init; }
     public bool IsInsert { get; set; }
-
-    internal void DeepCopy(out IEdit edit)
-    {
-        var serialized = JsonSerializer.Serialize(this);
-        edit = JsonSerializer.Deserialize<RowEdit<T>>(serialized) ?? throw new NullReferenceException(serialized);
-    }
 
     internal ColumnEdit(int parity, bool isInsert, int index, T header, IList<T?> values) : base(header, values)
     {
